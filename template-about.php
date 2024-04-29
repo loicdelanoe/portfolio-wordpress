a15a8cffa296e5b5d2a6b3d72866ad797f6b04e9
Loïc Delanoë <95628210+loicdelanoe@users.noreply.github.com>
2024-04-29 20:04:46 +0200

    Added responsive layout for about page

--- template-about.php
+++ template-about.php
@@ -36,8 +36,8 @@
                 </ul>
             </section>
         </div>
-        <section class="tools">
-            <h2 class="tools__title" data-animation="show-up"><span>Mes</span> Outils</h2>
+        <section class="tools" data-animation="show-up">
+            <h2 class="tools__title"><span>Mes</span> Outils</h2>
             <ul class="tools__container">
                 <li>
                     <img class="tools__item" src="<?= dw_asset('img/icons/css.svg') ?>" alt="Cascading Style Sheet">
@@ -110,32 +110,35 @@
             </ul>
         </section>
         <section class="approachs">
-            <h2 class="approachs__title"><span>Mon</span>Approche</h2>
+            <h2 class="approachs__title" data-animation="show-up"><span>Mon</span>Approche</h2>
             <ul class="approachs__container">
-                <li class="approachs__item approachs__item--design approach">
+                <li class="approachs__item approachs__item--design approach" data-animation="show-up">
                     <div class="approach__container">
                         <h3 class="approach__title">Design</h3>
-                        <p class="approach__content">Il est important de fournir un design complet et soigné aux clients afin qu’ils aient une bonne
+                        <p class="approach__content">Il est important de fournir un design complet et soigné aux clients
+                            afin qu’ils aient une bonne
                             idée de ce que leur site va donner quand il sera déployé.</p>
                     </div>
                 </li>
-                <li class="approachs__item approachs__item--dev approach">
+                <li class="approachs__item approachs__item--dev approach" data-animation="show-up">
                     <div class="approach__container">
                         <h3 class="approach__title">Développement</h3>
-                        <p class="approach__content">Après le design, le développement est tout aussi important, mais surtout le respect des normes / conventions des langages (sémantique en HTML).</p>
+                        <p class="approach__content">Après le design, le développement est tout aussi important, mais
+                            surtout le respect des normes / conventions des langages (sémantique en HTML).</p>
                     </div>
                 </li>
-                <li class="approachs__item approachs__item--access approach">
+                <li class="approachs__item approachs__item--access approach" data-animation="show-up">
                     <div class="approach__container">
                         <h3 class="approach__title">Accessibilité</h3>
-                        <p class="approach__content">L’accessibilité d’un site web est cruciale, en effet n’importe quel site internet doit être pensé aussi pour les personnes en situation de handicap.</p>
+                        <p class="approach__content">L’accessibilité d’un site web est cruciale, en effet n’importe quel
+                            site internet doit être pensé aussi pour les personnes en situation de handicap.</p>
                     </div>
                 </li>
             </ul>
         </section>
         <section class="talk">
-            <h2>Discutons :)</h2>
-            <a class="cta" href="/contact">Contactez-moi</a>
+            <h2 data-animation="show-up">Discutons :)</h2>
+            <a class="cta" href="/contact" data-animation="show-up">Contactez-moi</a>
         </section>
     </main>
 <?php get_footer() ?>
